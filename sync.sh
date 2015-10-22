ca82c0f8fc9540f3a2a230b04fc33f7f9da5e1b5
puppet-agent <puppet-agent@puppet.bps.wso2.com>
2015-10-22 04:02:15 -0400

    adding helper scripts.

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo service puppetmaster stop
-echo "----- Starting cleanup -----"
-sudo rm /etc/puppet/modules/ /etc/puppet/hieradata/ -rf
-echo "----- Copying Latest code -----"
-sudo cp manifests/  modules/  hieradata/ fileserver.conf  hiera.yaml  puppet.conf /etc/puppet/ -rf
-sudo service puppetmaster start
-sudo service puppetmaster status 
